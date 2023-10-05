c54d1c35b5e84ed272d26ff624f015f9801adae0
Guilherme Guimaraes <gillguimaraes@Guilhermes-MacBook-Pro-2.local>
2023-10-05 12:50:00 -0400

    first commit"
    "

---
+++ JavaScript/practice.js
@@ -0,0 +1,41 @@
+//console.log('hello world');
+
+//changing font and color 
+//console.log("%cHello World","color:blue; font-size:40px");
+
+
+//And operator '&&'
+//Using double amperstan to join to comparisons.
+// && returns of boolean value of either true or false. If both values on either side are true, the boolean return is true, otherwise it will return false
+// console.log(false && false);
+// let currentTime = 10;
+
+// console.log(currentTime > 10 && currentTime < 20);
+
+
+/* 
+    || operator "or"
+
+    The || operator checks to see if either comparison is true.
+    The || operator will ALWAYS return true, unless both evaulations are false. 
+
+*/
+// let newNumb = 12;
+// console.log( newNumb < 10 || newNumb > 100 );
+
+
+//Not operator !
+
+// Effectively flips the boolean value. ie:
+// let hungryDog = true;
+// console.log("the dog has been fed, is she still hungry?", !hungryDog); //returns 
+
+
+// The modulus operator returns the reaminder of a division.
+/* A restaurant has 5 tabels with 4 seats each.
+    22 guests arrive. How many guests will be without a seat?
+*/ 
+//  let restaurant = 5;
+//  let guests =22;
+
+//  console.log(guests % restaurant, "Guests will be without a seat");
