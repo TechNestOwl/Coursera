f1373f8c8e4ce898d3eb76ceeef30ae241c23f13
Guilherme Guimaraes <gillguimaraes@Guilhermes-MacBook-Pro-2.local>
2023-10-10 14:40:11 -0400

    objects

--- JavaScript/arrays_objects_functions.js
+++ JavaScript/arrays_objects_functions.js
@@ -7,19 +7,70 @@
 // D.R.Y. - Don't repeat yourself
 
 
-//simple function:
+//      Simple function:
 // function addNum(a,b){
 //     c = a + b;
 //     console.log(c);
 // }
 // addNum(2,4); // function invocation 'call'
 
-//          --- Looping Through an Array ---
-let arr = ["Eat","Sleep","Code","Repeat"];
 
-function listArrayItems(){
-    for(let i = 0; i < arr.length; i++){
-        console.log(arr[i]);
-    }
+
+//          --- Looping and Iterating With Functions  ---
+
+
+// let arr = ["Eat","Sleep","Code","Repeat"];
+// function listArrayItems(){
+//     for(let i = 0; i < arr.length; i++){
+//         if(arr[i] == "Sleep"){
+//             console.log(i, "Set alarm for 6:00am,", arr[i])
+//         }else{
+//             console.log(i + 1, arr[i]); //displaying the array item where the index is equal to 'i'
+//         }
+//     }
+// }
+// listArrayItems(arr);
+
+
+// function letterFinder(word,match){
+//     for(let i = 0; i < word.length; i++){
+//         if(word[i] == match){
+//             console.log("Found the", match, "at", i);
+//         }else {
+//             console.log("No match found at", i)
+//         }
+//     }
+// }
+// letterFinder("test","t");
+
+
+
+
+//          --- Objects and Dot Notation ---
+
+//creating videogame characters with two different methods
+let mainCharacter = {}; //creating an empty object
+
+mainCharacter.health = 100; // adding key: values, retroactively 
+mainCharacter.attack = 20;
+mainCharacter.defense = 50;
+mainCharacter.gold = 500;
+
+let goblinCharacter = {
+    health: 100,
+    attack: 15,
+    defense: 20,
+    gold: 100
+}; 
+
+// console.log("Hero -", mainCharacter);
+// console.log("Hero's health:", mainCharacter.health);
+
+let charactersHouse = {
+    value: 15000,
+    location: "City-center",
+    material: ["wood","stone","brick"]
 }
-listArrayItems(arr);
+console.log("Hero's house is made mostly of:",charactersHouse.material[1]);
+charactersHouse.windows = 4;
+console.log(charactersHouse);
