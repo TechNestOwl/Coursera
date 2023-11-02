3aba13a302657a229f31176272dc740f22bc3ef3
Guilherme Guimaraes <Technestowl@gmail.com>
2023-11-02 15:54:24 -0400

    default parameters

--- JavaScript/functionalProgramming.js
+++ JavaScript/functionalProgramming.js
@@ -112,3 +112,58 @@ vaycayArr.push("Outlet Mall")
 
 
 
+//          *** Default Parameters ***
+
+function noDefaultParams(number){
+    console.log("Result:", number * number);
+};
+
+//if I call the above function like this noDefaultParams(); --> Result will be NaN
+
+
+function withDefaultParams(number = 10){
+    console.log("Result:", number * number);
+};
+// default params allow me to build a function that will run with default argument values
+// even if I don't pass it any arguments.
+
+class NoDefaultParams {
+    constructor(num1, num2, num3, string1, bool1){
+        this.num1 = num1;
+        this.num2 = num2;
+        this.num3 = num3;
+        this.string1 = string1;
+        this.bool1 = bool1;
+    }
+    calculate(){
+        if(this.bool1){
+            console.log(this.string1, this.num1 + this.num2 + this.num3);
+            return;
+        }
+        return "The value of bool1 is incorrect"
+    }
+};
+
+let fail = new NoDefaultParams(1,2,3,false);
+fail.calculate(); // the value of bool1 is incorrect
+
+class WithDefaultParams {
+    constructor(num1 = 1, num2 = 2, num3 = 3, string1 = "Result:", bool1=true){
+        this.num1 = num1;
+        this.num2 = num2;
+        this.num3 = num3;
+        this.string1 = string1;
+        this.bool1 = bool1;
+    }
+    calculate(){
+        if(this.bool1){
+            console.log(this.string1, this.num1 + this.num2 + this.num3);
+            return;
+        }
+        return "The value of bool1 is incorrect"
+    }
+};
+
+let betterEx = new WithDefaultParams();
+
+betterEx.calculate();
