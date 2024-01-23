136d94a51b64a9adb0faa242634fcf285785e6a3
Guilherme Guimaraes <Technestowl@gmail.com>
2024-01-23 17:30:01 -0500

    starting review files

---
+++ JavaScript/Review/longestWord2.js
@@ -0,0 +1,14 @@
+let names = [
+    "Ant Man",
+    "Dog Man",
+    "Superman",
+    "Storm",
+    "Frodo",
+    "Jake"
+];
+
+//write a functio that that takes in an araray an returns the longest name.
+
+function longestName(arr){
+    
+}
