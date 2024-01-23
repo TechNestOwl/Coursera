e80fd9ffff739f337dfcc28ae34bdb823a7d72af
Guilherme Guimaraes <Technestowl@gmail.com>
2024-01-23 18:06:48 -0500

    added km to mi conversion function

---
+++ JavaScript/Review/kmToMi.js
@@ -0,0 +1,14 @@
+// write a function that takes a number parameter in ki
+// converts the number to miles. Return the converted number.
+
+
+function kmToMi(km){
+
+    let conversionToMi = 0;
+
+    conversionToMi = (km * .621);
+
+    return conversionToMi
+}
+
+console.log(kmToMi(100));
