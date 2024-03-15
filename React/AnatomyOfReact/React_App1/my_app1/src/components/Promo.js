e63632869f25480ebddcc8f89969c45b77346799
Guilherme Guimaraes <Technestowl@gmail.com>
2024-03-15 17:45:49 -0400

    added components to reactApp1

---
+++ React/AnatomyOfReact/React_App1/my_app1/src/components/Promo.js
@@ -0,0 +1,12 @@
+export default function Promo(){
+    return (
+        <div className="promo-section">
+            <div>
+                <h1>Don't Miss This Deal!</h1>
+            </div>
+            <div>
+                <h2>Subscribe to my newsletter and get all the shop items at 40% off!</h2>
+            </div>
+        </div>
+    );
+};
