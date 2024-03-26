6f91ac3cf54a24ed5d22d7215dedcc35a1af55b0
Guilherme Guimaraes <Technestowl@gmail.com>
2024-03-26 11:35:43 -0400

    added heading comp, as well as notes on JSX

--- React/AnatomyOfReact/React_App1/my_app1/src/App.js
+++ React/AnatomyOfReact/React_App1/my_app1/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Heading from './components/Heading'
 
 function App() {
   return (
     <div className="App">
+      <Heading name="Bob"/>
      
     </div>
   );
---
+++ React/AnatomyOfReact/React_App1/my_app1/src/components/Heading.js
@@ -0,0 +1,9 @@
+function Heading(props){
+    return(
+        <div>
+            <p>Hello {props.name}</p>
+        </div>
+    )
+};
+
+export default Heading;
